00ae43a225cf23fd3ed937f27ab25aba8d812db9
Greg Karékinian <greg@karekinian.com>
2016-04-15 18:00:51 +0200

    Don't specify a provider for the service

--- recipes/install.rb
+++ recipes/install.rb
@@ -42,15 +42,9 @@ else
   php_fpm_service_name = node['php-fpm']['service_name']
 end
 
-service_provider = nil
-if node['platform'] == 'ubuntu' and node['platform_version'].to_f >= 13.10
-  service_provider = ::Chef::Provider::Service::Upstart
-end
-
 directory node['php-fpm']['log_dir']
 
 service "php-fpm" do
-  provider service_provider if service_provider
   service_name php_fpm_service_name
   supports :start => true, :stop => true, :restart => true, :reload => true
   action [ :enable, :start ]
